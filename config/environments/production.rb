99dcd87f984b3f4041ec853ecd1f32fc2610143a
Garrick Lau <garrick.lau@trustfreightglobal.com>
2025-03-04 13:36:40 -0500

    updating port setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 8080 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
